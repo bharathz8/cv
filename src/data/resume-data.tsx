26c7c9f51685da013a4737301e0234c41adeb9f0
bharath <164410230+bharathz8@users.noreply.github.com>
2024-12-13 12:20:26 +0530

    Update resume-data.tsx
    updated links

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -136,7 +136,7 @@ export const RESUME_DATA = {
         "SiteCraftor is an AI-powered platform that automates website creation, allowing users to quickly build professional websites with ease. It streamlines the design and deployment process, making website building accessible to everyone.",
       link: {
         label: "site Craftor",
-        href: "https://github.com/bharathz8/sitecraftor",
+        href: "https://github.com/bharathz8/website-builder",
       },
     },
     {
